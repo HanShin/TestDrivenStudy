459aea7231415b992b4fcca30d148abd190f8786
HanShin <vlrkwlepd@gmail.com>
2015-04-28 15:06:09 +0900

    state-based test2

---
+++ TestDrivenStudy/UnitTestStart_Chapter2.Tests/CalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestStart_Chapter2.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        Calculator calc = null;
+        [TestInitialize]
+        public void Setup()
+        {
+            calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void Sum_NoAddCalls_DefaultsToZero()
+        {
+            int lastSum = calc.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [TestMethod]
+        public void Add_CalledOnce_SavesNumberForSum()
+        {
+            calc.Add(1);
+            int lastSum = calc.Sum();
+            Assert.AreEqual(1, lastSum);
+        }
+    }
+}
