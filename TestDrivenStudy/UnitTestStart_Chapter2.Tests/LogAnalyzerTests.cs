f4b0eadb84aa4db9191e6167014c8a919274af5a
HanShin <vlrkwlepd@gmail.com>
2015-04-28 13:34:42 +0900

    테스트 프레임워크사용

---
+++ TestDrivenStudy/UnitTestStart_Chapter2.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestStart_Chapter2;
+
+namespace UnitTestStart_Chapter2.Tests
+{
+    [TestClass]
+    public class LogAnalyzerTests
+    {
+        [TestMethod]
+        public void IsValidFileName_validFileLowerCased_ReturnsTrue()
+        {
+            // 준비
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            // 작용을 가함
+            bool result = analyzer.IsValidLogFileName("whatever.slf");
+
+            // assert
+            Assert.IsTrue(result, "파일 이름이 적절하지 않음!");
+        }
+    }
+}
---
+++ TestDrivenStudy/UnitTestStart_Chapter2/LogAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestStart_Chapter2
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.ToLower().EndsWith(".slf"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
