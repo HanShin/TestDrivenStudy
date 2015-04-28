f0a97d5ef1ca993eaf8fc1afcad45edf1329c6aa
HanShin <vlrkwlepd@gmail.com>
2015-04-28 15:42:28 +0900

    하부 구현을 교체할 수 있도록 인터페이스를 추출. 파일시스템을 건드리는 클래스를 추출한 후 호출.

---
+++ TestDrivenStudy/Stub_Chapter3/FileExtensionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stub_Chapter3
+{
+    public class FileExtensionManager
+    {
+        public bool IsValid(string fileName)
+        {
+            // 파일을 읽어드린다.
+        }
+    }
+}
---
+++ TestDrivenStudy/Stub_Chapter3/LogAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stub_Chapter3
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            FileExtensionManager mgr = new FileExtensionManager();
+            return mgr.IsValid(fileName);
+        }
+    }
+}
