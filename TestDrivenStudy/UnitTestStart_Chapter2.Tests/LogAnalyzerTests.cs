1f760e456ba7e0bd8c58848ae4d6f074f2a6f41c
HanShin <vlrkwlepd@gmail.com>
2015-04-28 13:40:25 +0900

    SetUp과 TearDown

--- TestDrivenStudy/UnitTestStart_Chapter2.Tests/LogAnalyzerTests.cs
+++ TestDrivenStudy/UnitTestStart_Chapter2.Tests/LogAnalyzerTests.cs
@@ -7,17 +7,40 @@ namespace UnitTestStart_Chapter2.Tests
     [TestClass]
     public class LogAnalyzerTests
     {
+        private LogAnalyzer m_analyers = null;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            m_analyers = new LogAnalyzer();
+        }
+
         [TestMethod]
         public void IsValidFileName_validFileLowerCased_ReturnsTrue()
         {
-            // 준비
-            LogAnalyzer analyzer = new LogAnalyzer();
 
             // 작용을 가함
-            bool result = analyzer.IsValidLogFileName("whatever.slf");
+            bool result = m_analyers.IsValidLogFileName("whatever.slf");
 
             // assert
             Assert.IsTrue(result, "파일 이름이 적절하지 않음!");
+        }
+
+        [TestMethod]
+        public void IsValidFileName_validFileUpperCased_ReturnsTrue()
+        {
+
+            // 작용을 가함
+            bool result = m_analyers.IsValidLogFileName("whatever.SLF");
+
+            // assert
+            Assert.IsTrue(result, "파일 이름이 적절하지 않음!");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            m_analyers = null;
         }
     }
 }
