a154461edca633448fdfcaf10f6d1f5ab375ef8d
HanShin <vlrkwlepd@gmail.com>
2015-04-28 09:07:28 +0900

    SimpleParserTest

---
+++ TestDrivenStudy/UnitTestStart_Chapter1/Code/SimpleParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestStart_Chapter1.Code
+{
+    class SimpleParser
+    {
+        public int ParseAndSum(string numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+            if (!numbers.Contains(","))
+            {
+                return int.Parse(numbers);
+            }
+            else
+            {
+                throw new InvalidOperationException("지금은 0개나 1개 숫자밖에 처리못함");
+            }
+        }
+    }
+}
---
+++ TestDrivenStudy/UnitTestStart_Chapter1/TestClass/SimpleParserTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestStart_Chapter1.Code;
+
+namespace UnitTestStart_Chapter1.TestClass
+{
+    class SimpleParserTests
+    {
+        public static void TestReturnsZeroWhenEmptyString()
+        {
+            try
+            {
+                SimpleParser p = new SimpleParser();
+                int result = p.ParseAndSum(string.Empty);
+                if (result != 0)
+                {
+                    Console.WriteLine(@"SimplePArserTests.TestReturnsZeroWhenEmptyString: ParseAndSum은 빈 문자열에 대해 0을 반환해야한다.");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
