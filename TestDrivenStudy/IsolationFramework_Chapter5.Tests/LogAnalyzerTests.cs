6d04ea7d67c70e4df1cd8839114811662a8f7cd7
HanShin <vlrkwlepd@gmail.com>
2015-04-29 16:22:35 +0900

    직접 작성한 목 객체(동적 목 개체로 변경할 예정)

---
+++ TestDrivenStudy/IsolationFramework_Chapter5.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MockObject_Chapter5;
+
+namespace IsolationFramework_Chapter5.Tests
+{
+    [TestClass]
+    public class LogAnalyzerTests
+    {
+        [TestMethod]
+        public void Analyze_TooShortFileName_CallsWebService()
+        {
+            ManualMockService mockService = new ManualMockService();
+            LogAnalyzer log = new LogAnalyzer(mockService);
+            string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+            Assert.AreEqual("Filename too short : abc.ext", mockService.LastError);
+        }
+    }
+}
---
+++ TestDrivenStudy/IsolationFramework_Chapter5.Tests/ManualMockService.cs
@@ -0,0 +1,18 @@
+﻿using MockObject_Chapter5;
+
+namespace IsolationFramework_Chapter5.Tests
+{
+    public class ManualMockService : IWebService
+    {
+        public string LastError;
+
+        #region IWebService Members
+
+        public void LogError(string message)
+        {
+            LastError = message;
+        }
+
+        #endregion
+    }
+}
