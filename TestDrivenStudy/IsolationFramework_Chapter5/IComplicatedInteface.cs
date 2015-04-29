0ab37651b20a52cc32e78f3465a09094a9821c62
HanShin <vlrkwlepd@gmail.com>
2015-04-29 15:52:59 +0900

    수작업으로 스텁을 이용한 복잡한 인터페이스 구현

---
+++ TestDrivenStudy/IsolationFramework_Chapter5/IComplicatedInteface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolationFramework_Chapter5
+{
+    interface IComplicatedInteface
+    {
+        void Method1(string a, string b, bool c, int x, object o);
+        void Method2(string b, bool c, int x, object o);
+        void Method3(bool c, int x, object o);
+    }
+}
