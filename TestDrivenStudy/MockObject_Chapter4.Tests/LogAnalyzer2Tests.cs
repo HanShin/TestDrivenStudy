9a0e9cbffc54006c1e178d0ca15b1912ca6a9e6a
HanShin <vlrkwlepd@gmail.com>
2015-04-29 14:39:59 +0900

    목과 스텁을 함께 사용하기.

---
+++ TestDrivenStudy/MockObject_Chapter4.Tests/LogAnalyzer2Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MockObject_Chapter4.Tests
+{
+    [TestClass]
+    public class LogAnalyzer2Tests
+    {
+        [TestMethod]
+        public void Analyze_WebServiceThrows_SendsEmail()
+        {
+            StubService stubService = new StubService();
+            stubService.ToThrow = new Exception("fake exception");
+
+            MockEmailService mockEmail = new MockEmailService();
+
+            LogAnalyzer2 log = new LogAnalyzer2();
+
+            log.Service = stubService;
+            log.Email = mockEmail;
+
+            string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+            Assert.AreEqual("a",mockEmail.To);
+            Assert.AreEqual("fake exception",mockEmail.Body);
+            Assert.AreEqual("subject",mockEmail.Subject);
+        }
+    }
+}
---
+++ TestDrivenStudy/MockObject_Chapter4/StubService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockObject_Chapter4
+{
+    public class StubService : IWebService
+    {
+        public Exception ToThrow;
+        #region IWebService Members
+
+        public void LogError(string message)
+        {
+            throw ToThrow;
+        }
+
+        #endregion
+    }
+}
