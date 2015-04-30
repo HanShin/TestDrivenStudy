d4b4e4ffea26a300f1421862cdb699449c23b5b2
HanShin <vlrkwlepd@gmail.com>
2015-04-30 16:42:55 +0900

    이벤트가 등록되었는지 테스트하기

---
+++ TestDrivenStudy/IsolationFramework_Chapter5.Tests/PresenterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace IsolationFramework_Chapter5.Tests
+{
+    [TestClass]
+    public class PresenterTests
+    {
+        [TestMethod]
+        public void VerifyAttachesToViewEvents()
+        {
+            MockRepository mocks = new MockRepository();
+            IView viewMock = (IView)mocks.CreateMock(typeof(IView));
+
+            using (mocks.Record())
+            {
+                viewMock.Load += null;
+                LastCall.IgnoreArguments();
+            }
+            new Presenter(viewMock);
+            mocks.VerifyAll();
+
+        }
+    }
+}
---
+++ TestDrivenStudy/IsolationFramework_Chapter5/Presenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolationFramework_Chapter5
+{
+    public class Presenter
+    {
+        IView view;
+
+        public Presenter(IView view)
+        {
+            this.view = view;
+            this.view.Load += new EventHandler(view_Load);
+        }
+
+        void view_Load(object sender, EventArgs e)
+        {
+            throw new Exception("Not implemented.");
+        }
+    }
+}
